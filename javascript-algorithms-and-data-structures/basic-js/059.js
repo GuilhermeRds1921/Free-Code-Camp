a5ff0f28f13db7a4abfd9888b3ae6b9a539736c7
GuilhermeRds1921 <guilhermerodrigues1921@gmail.com>
2023-07-27 09:36:28 -0300

    ex 64

---
+++ javascript-algorithms-and-data-structures/basic-js/059.js
@@ -0,0 +1,15 @@
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  if(wasThatTrue){
+    const val = "Yes, that was true";
+    return val;
+  }
+  else{
+    const val = "No, that was false";
+    return val;
+  }
+  
+  
+    // Only change code above this line
+  
+  }
